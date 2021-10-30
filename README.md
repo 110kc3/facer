ac9f4f543809c409d2a95f8464956a6585ab57f5
Kamil Choiński <110kc3@gmail.com>
2021-10-31 01:43:28 +0200

    deleting aws, new face detector code

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
 # facer
 
 
-Program for now uses Stump-based 24x24 discrete adaboost frontal face detector.
+
+
+face_detector_only.py uses Stump-based 24x24 discrete adaboost frontal face detector.
 haarcascade_frontalface_default.xml file for face detection
 
-Application address:  http://facer-test.eu-west-2.elasticbeanstalk.com/
 
-
-Files description:
-.ebextensions - required for aws deployment
-
-
-AWS Cloud:
-
-installing tool for deployment:
-https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/eb-cli3-install-windows.html
-
-
-eb init -p python-3.7 flask-tutorial --region eu-west-2
---
+++ face_detector_only.py
@@ -0,0 +1,27 @@
+import cv2
+camera = cv2.VideoCapture(0)  # camera number defined - not sure why 0
+
+
+# face detection
+faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+def generate_frames():
+    while True:
+
+        # read the camera frame
+        success, frame = camera.read()
+        facesDetected = faceDetect.detectMultiScale(frame, 1.3, 5)
+        for x, y, w, h in facesDetected:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 2)
+        if not success:
+            break
+        else:
+            temp, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+        # I'm not sure what this does, but the camera does not work without it
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpg\r\n\r\n' + frame + b'\r\n')
+
+
