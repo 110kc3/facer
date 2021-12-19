2be3a513750f216a29a488b3ca27f4c614db428f
Oskar Stabla <oskar.stabla@euvic.pl>
2021-12-19 14:47:36 +0100

    fix: disable warnings

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_BINDS'] = ""
 db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
