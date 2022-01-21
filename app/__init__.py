691b558147bffdfdf4f252896e1be195907a20ab
jeffbebe <oskarw46@gmail.com>
2022-01-21 15:13:38 +0100

    t

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+from app import views  # important to retrieve routes despite not being strictly used, must be in last line, otherwise routes will not be retrieved
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 app.config['SECRET_KEY'] = 'super secret key'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_BINDS'] = ""
@@ -14,5 +17,3 @@ app.config['UPLOAD_PATH'] = 'app/static/images'
 db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
-
-from app import views # important to retrieve routes despite not being strictly used, must be in last line, otherwise routes will not be retrieved     
