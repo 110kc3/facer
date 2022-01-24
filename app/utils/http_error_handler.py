a4c9479f3fa185ca99fb13a43f16e66a2e678835
jeffbebe <oskarw46@gmail.com>
2022-01-24 14:45:34 +0100

    t

--- app/utils/http_error_handler.py
+++ app/utils/http_error_handler.py
@@ -3,8 +3,8 @@ import json
 
 def http_error_handler(error):
     if(not isinstance(error, ValueError)):
-        return json.dump(str(error)), 500
+        return str(error), 500
     json_error = json.loads(str(error))
     code = json_error["code"]
     message = json_error["message"]
-    return json.dump(message), code
+    return message, code
